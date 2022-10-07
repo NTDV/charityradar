ffc77408e073839525d5ea1bc74aa519bc2cb138
Данила Вальковец <dahnh33@gmail.com>
2022-10-07 18:37:18 +0300

    Add site

---
+++ app.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    // Fixed Header
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    });
+
+    //Scrolling
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll')
+        let elementOffset = $(elementID).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 80
+        }, 600);
+    });
+});
