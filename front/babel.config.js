db1cefa20849110e5e1fc6b06a6ec0e8aa3faca1
Данила Вальковец <dahnh33@gmail.com>
2022-10-07 03:52:02 +0300

    Revert "Trying to fix module visibilty"
    This reverts commit df79123059df0a55e2bc070c44d9fbc945e0786e.

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,33 +1,6 @@
-const tsconfig = require('./tsconfig.paths.json');
-let rawAlias   = tsconfig.compilerOptions.paths;
-let alias      = {};
-
-for (let x in rawAlias) {
-  alias[x.replace('/*', '')] = rawAlias[x].map(
-      p => p.replace('/*', ''));
-}
-
 module.exports = function(api) {
   api.cache(true);
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: [
-            '.ios.js',
-            '.android.js',
-            '.js',
-            '.ts',
-            '.tsx',
-            '.json',
-          ],
-          alias,
-        },
-      ],
-    ],
   };
 };
