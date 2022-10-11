d7a3e5a34b15d59047e850edad83acba7cfac5a9
Данила Вальковец <dahnh33@gmail.com>
2022-10-12 01:24:17 +0300

    VTB ID token by credentials

---
+++ back/api/src/main/java/ru/charityradar/api/dto/VTBMasterToken.java
@@ -0,0 +1,43 @@
+package ru.charityradar.api.dto;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.List;
+
+public record VTBMasterToken(String access_token, Integer expires_in, Integer refresh_expires_in, String token_type, String scope) {
+    @SuppressWarnings("CopyConstructorMissesField")
+    public VTBMasterToken(final VTBMasterToken masterToken) throws NullPointerException {
+        this(masterToken.access_token(), masterToken.expires_in(), masterToken.refresh_expires_in(), masterToken.token_type(), masterToken.scope());
+    }
+
+    public static VTBMasterToken generate(final String login, final String password) throws AuthenticationException {
+        final var headers = new HttpHeaders();
+        headers.setBasicAuth(login, password);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        final var body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "client_credentials");
+        final var response = new RestTemplate()
+                .postForEntity("https://auth.bankingapi.ru/auth/realms/kubernetes/protocol/openid-connect/token", new HttpEntity<>(body, headers) , VTBMasterToken.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            final var masterToken = response.getBody();
+            if (masterToken != null) {
+                try {
+                    return new VTBMasterToken(masterToken);
+                } catch (final NullPointerException e) {
+                    throw new AuthenticationException(HttpStatus.BAD_REQUEST.getReasonPhrase());
+                }
+            } else {
+                throw new AuthenticationException(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            }
+        } else {
+            throw new AuthenticationException(response.getStatusCode().getReasonPhrase());
+        }
+    }
+}
