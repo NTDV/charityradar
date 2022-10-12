e10ff81f9bdd1f3d0dabcd39de242ee43d646871
Данила Вальковец <dahnh33@gmail.com>
2022-10-12 22:28:49 +0300

    Properties improvements

--- back/api/src/main/java/ru/charityradar/api/helper/ProjectProperties.java
+++ back/api/src/main/java/ru/charityradar/api/helper/ProjectProperties.java
@@ -1,20 +1,60 @@
 package ru.charityradar.api.helper;
 
-import javax.mail.MessagingException;
-import java.io.FileInputStream;
+import lombok.ToString;
+
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ProjectProperties {
-    public static Properties properties = new Properties();
+    @SuppressWarnings("SpellCheckingInspection")
+    @ToString
+    public enum ProjectProperty {
+        MAIL_SENDER_LOGIN("mail.sender.login"),
+        MAIL_SENDER_PASSWORD("mail.sender.password"),
+        PROGRAMMERS_MAILS("programmers.mails"),
+
+        IMAGE_UPLOAD_PATH("image.upload.path"),
+        MAIN_URL("main.url"),
+
+        AUTH_VTBID_MASTERTOKEN_URL("auth.vtbid.mastertoken.url"),
+        AUTH_VTBID_ACCESSTOKEN_URL("auth.vtbid.accesstoken.url"),
+        ;
+
+        private final String _key;
+        private String _cachedValue;
+
+        ProjectProperty(final String key) {
+            _key = key;
+        }
+
+        public String getKey() {
+            return _key;
+        }
+
+        public String getValue(final Properties properties) {
+            if (properties == null) return _cachedValue = getProperties().getProperty(_key);
+            return _cachedValue = properties.getProperty(_key);
+        }
+
+        public String getValue() {
+            return getValue(_properties);
+        }
+
+        public String getCachedValue() {
+            if (_cachedValue == null) return _cachedValue = getValue(_properties);
+            return _cachedValue;
+        }
+    }
+
+    private static Properties _properties = new Properties();
 
     public static Properties getProperties(){
         try (InputStream input = ProjectProperties.class.getClassLoader().getResourceAsStream("main.properties")) {
-            properties = new Properties();
-            properties.load(input);
+            _properties = new Properties();
+            _properties.load(input);
         } catch (Exception e) {
             System.out.println("Error: Ошибка чтения файла main.properties");
         }
-        return properties;
+        return _properties;
     }
 }
